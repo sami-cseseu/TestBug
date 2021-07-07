6689301cbee4cfdd7769faab8d95bbb2338c4e78
Sami <sami.cseseu@gmail.com>
2021-07-07 13:42:49 +0200

    Added main file in task1

---
+++ src/task1/Main.java
@@ -0,0 +1,11 @@
+package task1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FileProcessor fp = new FileProcessor();
+		WebSession s = new WebSession();
+		fp.processInsert(s);
+	}
+
+}
